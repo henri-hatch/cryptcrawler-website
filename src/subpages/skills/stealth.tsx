7cb034379eb005dfa1aa5dee5145e6229259bbb0
Henri Hatch <henrihatch@gmail.com>
2025-03-25 02:23:55 -0500

    Add stealth maneuvers

--- src/subpages/skills/stealth.tsx
+++ src/subpages/skills/stealth.tsx
@@ -7,32 +7,45 @@ const Stealth = () => {
 
             <div>
             <h3>Cunning Dodge</h3>
-            <p dir="auto"><em>Lightning fast reactions allow you to nimbly duck and dodge most attacks.</em></p>
-            <p dir="auto">You gain +2d12 to your dodge score.</p>
             </div>
+            <div>
+            <img src="/manuever-images/Cunning Dodge.png" alt="Cunning Dodge" style={{ width: '75%', height: 'auto'}} />
+            </div>
+
+            <hr style={{ opacity: 0.2 }} />
 
             <div>
             <h3>Evasion</h3>
-            <p dir="auto"><em>You can nimbly dodge most area of effect attacks.</em></p>
-            <p dir="auto">When an effect forces a DEX saving throw to take half damage, you take no damage if you succeed, and only half damage if you fail.</p>
             </div>
+            <div>
+            <img src="/manuever-images/Evasion.png" alt="Evasion" style={{ width: '75%', height: 'auto'}} />
+            </div>
+
+            <hr style={{ opacity: 0.2 }} />
 
             <div>
             <h3>Improvised Cover</h3>
-            <p dir="auto"><em>Improvising camoflauge, you are able to conceal your location even after attacking.</em></p>
-            <p dir="auto">While stealthed, your ranged attack rolls do not break stealth as long as you are 60ft or farther from your target.</p>
             </div>
+            <div>
+            <img src="/manuever-images/Improvised Cover.png" alt="Improvised Cover" style={{ width: '75%', height: 'auto'}} />
+            </div>
+
+            <hr style={{ opacity: 0.2 }} />
 
             <div>
             <h3>Sneak Attack</h3>
-            <p dir="auto"><em>You can easily take advantage of openings and weaknesses in your opponent's defenses.</em></p>
-            <p dir="auto">Whenever you perform an attack roll with advantage or if you are attacking a creature adjacent to a creature friendly to you, you deal 2d6 extra damage.</p>
             </div>
+            <div>
+            <img src="/manuever-images/Sneak Attack.png" alt="Sneak Attack" style={{ width: '75%', height: 'auto'}} />
+            </div>
+
+            <hr style={{ opacity: 0.2 }} />
 
             <div>
             <h3>Steady Shot</h3>
-            <p dir="auto"><em>Collecting your breath, you focus your attention into a single shot.</em></p>
-            <p dir="auto">[1/turn]: As a bonus action, you gain advantage on your next ranged attack.</p>
+            </div>
+            <div>
+            <img src="/manuever-images/Steady Shot.png" alt="Steady Shot" style={{ width: '75%', height: 'auto'}} />
             </div>
         </>
     )
