49afbdbb7d94b1086cb87743d8abf166fb5b27b0
Henri Hatch <henrihatch@gmail.com>
2025-04-03 01:03:52 -0500

    Switch classes over to the same system

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -1,7 +1,12 @@
 import ContentCard from '../components/content_card';
+import classData from '../data/class_data';
 import '../components/content_card.css';
 
 const ClassesPage = () => {
+  // Filter classes by category
+  const commonClasses = classData.filter(classItem => classItem.category === "common");
+  const rareClasses = classData.filter(classItem => classItem.category === "rare");
+  
   return (
     <>
       <h2>Character Classes</h2>
@@ -9,50 +14,30 @@ const ClassesPage = () => {
       
       <h3>Common Classes</h3>
       <div className="cards-container">
-      <ContentCard 
-          title="Conduit" 
-          imagePath="/class-images/conduit_logo.png" 
-          linkTo="/classes/conduit"
-          description="Despite their promises, Gods and entities of beyond still interfere in the mortal world."
-        /> 
-
-      <ContentCard 
-          title="Minstrel" 
-          imagePath="/class-images/minstrel_logo.png" 
-          linkTo="/classes/minstrel"
-          description="Minstrels are poets, the authors of myth, writing the very adventure the party is in."
-        /> 
-
-      <ContentCard 
-          title="Paragon" 
-          imagePath="/class-images/paragon_logo.png" 
-          linkTo="/classes/paragon"
-          description="Bound by a powerful oath, Paragons seek to become the ideal they have sworn to."
-        />
-
-        <ContentCard 
-          title="Shadow" 
-          imagePath="/class-images/shadow_logo.png" 
-          linkTo="/classes/shadow"
-          description="Masters of thievery and sneak, Shadows excel from, well, the shadows."
-        />
-        
-        <ContentCard
-          title="Tactician"
-          imagePath="/class-images/tactician_logo.png"
-          linkTo="/classes/tactician"
-          description="Tacticians are the masterminds of the battlefield, using strategy and cunning to outwit their foes."
-        />
+        {commonClasses.map(classItem => (
+          <ContentCard 
+            key={classItem.id}
+            id={classItem.id}
+            title={classItem.name}
+            imagePath={classItem.imagePath}
+            linkTo={classItem.pageRoute}
+            description={classItem.description}
+          />
+        ))}
       </div>
 
       <h3>Rare Classes</h3>
       <div className="cards-container">
-      <ContentCard 
-          title="Abnegate" 
-          imagePath="/class-images/null_logo.png" 
-          linkTo="/classes/abnegate"
-          description="Born as a twin, an Abnegate is an anti-magical anomaly, blessed with supernatural physical power rather than magical gifts."
-        /> 
+        {rareClasses.map(classItem => (
+          <ContentCard 
+            key={classItem.id}
+            id={classItem.id}
+            title={classItem.name}
+            imagePath={classItem.imagePath}
+            linkTo={classItem.pageRoute}
+            description={classItem.description}
+          />
+        ))}
       </div>
     </>
   );
