bd5e9529635eb308835c8eed6c2e744d5804bb43
Henri Hatch <henrihatch@gmail.com>
2025-04-03 17:59:05 -0500

    Search functionality

--- src/data/item_data.tsx
+++ src/data/item_data.tsx
@@ -4,6 +4,7 @@ interface ItemData {
     gpCost: number;
     weight: number;
     category: string[]; // Can be any number of: "equipment", "consumable", "weapon", "armor", "tool", "adventuring_gear", "magic_item"
+    description: string;
     maneuverImage?: string;
 }
 
@@ -13,6 +14,7 @@ const itemData: ItemData[] = [
         name: "Abacus",
         gpCost: 2,
         weight: 4,
+        description: "A counting frame with beads, used for calculations and accounting.",
         category: ["equipment"],
     },
     {
@@ -20,6 +22,7 @@ const itemData: ItemData[] = [
         name: "Acid (Vial)",
         gpCost: 25,
         weight: 1,
+        description: "A vial of corrosive acid that can be used to dissolve materials or as a weapon.",
         category: ["equipment", "consumable"],
     },
     {
@@ -27,6 +30,7 @@ const itemData: ItemData[] = [
         name: "Alchemist Fire",
         gpCost: 50,
         weight: 1,
+        description: "A sticky, flammable substance that ignites on contact with air. It can be thrown as a weapon or used to create a fire.",
         category: ["equipment", "consumable"],
     },
     {
@@ -34,6 +38,7 @@ const itemData: ItemData[] = [
         name: "Alchemist Supplies",
         gpCost: 50,
         weight: 10,
+        description: "A set of tools and ingredients used by alchemists to create potions, poisons, and other chemical concoctions.",
         category: ["equipment", "tool"],
     }
 ]
