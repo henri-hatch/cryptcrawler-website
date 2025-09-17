c9172d1476ab271754a2fe180efe66fa55b661e5
Henri Hatch <henrihatch@gmail.com>
2025-09-17 17:37:09 -0500

    Fix unarmed skill tree

--- src/subpages/skills/unarmed.tsx
+++ src/subpages/skills/unarmed.tsx
@@ -1,29 +1,14 @@
-import { createManeuverSkillSlot, createMasterySkillSlot } from '../../utils/skill_utils';
+import { createManeuverSkillSlot } from '../../utils/skill_utils';
 import SkillTemplate from '../../components/skill_template';
 
 const Unarmed = () => {
     const row1 = [
-        createManeuverSkillSlot("fisticuffs"),
-        createManeuverSkillSlot("counterstrike"),
-        createManeuverSkillSlot("titan-kick"),
-        createManeuverSkillSlot("return-to-sender"),
-        createManeuverSkillSlot("not-surprised"),
-    ]
-    const row2 = [
-        createManeuverSkillSlot("delayed-strike"),
-        createManeuverSkillSlot("wind-step-palm"),
-        createManeuverSkillSlot("sucker-punch"),
-    ]
-    const row4 = [
-
-        createMasterySkillSlot("hercules", "/mastery-images/mastery-logos/HerculesMastery.png", "Hercules"),
+        createManeuverSkillSlot("flurry-of-blows"),
     ]
     return (
         <SkillTemplate 
             skillName="Unarmed" 
             row1 = {row1}
-            row2 = {row2}
-            row4 = {row4}
         />
     );
 };
